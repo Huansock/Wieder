e3d95bbab334e288d0944a51e6c70826358ca3b6
Schlingel <Huan.sock@gmail.com>
2023-10-21 13:28:48 +0200

    bun.js , svelte, superbase init

---
+++ wieder-sveltekit/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+  import { createClient } from '@supabase/supabase-js'
+
+  export const supabase = createClient('https://edeygehidkmpalduqmws.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkZXlnZWhpZGttcGFsZHVxbXdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc4ODI2NTksImV4cCI6MjAxMzQ1ODY1OX0.7dxSQGj8iUMe8Ezm3-8QEwSDmTzC07p9SWapseijL_Y')
